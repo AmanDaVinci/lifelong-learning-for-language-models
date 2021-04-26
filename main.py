e182e00b2823e2b7396a42f2afbcbd73f5191ede
amandavinci <amanthevinci@gmail.com>
2021-04-26 14:10:20 +0200

    add replay trainer to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
 
 from LL4LM.trainers.lifelong_trainer import LifelongTrainer
+from LL4LM.trainers.lifelong_trainer import ReplayTrainer
 from LL4LM.trainers.multitask_trainer import MultitaskTrainer
 from LL4LM.trainers.mixture_of_experts_trainer import MixtureOfExpertsTrainer
 from LL4LM.trainers.datastream_scanner import DatastreamScanner
@@ -15,6 +16,8 @@ def main(config: DictConfig):
     with wandb.init(project="LL4LM", config=dict_config):
         if config.trainer == "LifelongTrainer":
             trainer = LifelongTrainer(config)
+        elif config.trainer == "ReplayTrainer":
+            trainer = ReplayTrainer(config)
         elif config.trainer == "MultitaskTrainer":
             trainer = MultitaskTrainer(config)
         elif config.trainer == "MixtureOfExpertsTrainer":
