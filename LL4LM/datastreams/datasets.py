d904a6cad75b9040625b20df4a48d268646fc4fe
amandavinci <amanthevinci@gmail.com>
2021-04-29 08:44:20 +0200

    add all other datasets

--- LL4LM/datastreams/datasets.py
+++ LL4LM/datastreams/datasets.py
@@ -35,7 +35,9 @@ dataset_configs = {
         "test_split": "test", 
         "transform": amazon_reviews
     },
+    # --------------------------------------------------------------------------
     # multidomain
+    # --------------------------------------------------------------------------
     "reviews_home": {
         "path": "amazon_reviews_multi",
         "name": "en", 
@@ -89,5 +91,128 @@ dataset_configs = {
         "filter_column": "product_category",
         "filter_value": "kitchen",
         "transform": amazon_reviews
+    },
+    # --------------------------------------------------------------------------
+    # NLI multilingual
+    # --------------------------------------------------------------------------
+    "mnli": {
+        "path": "glue",
+        "name": "mnli", 
+        "train_split": "train", 
+        "test_split": "validation_matched", 
+        "transform": mnli
+    },
+    "xnli": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "transform": xnli
+    },
+    "xnli_ar": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "filter_column": "language",
+        "filter_value": "ar",
+        "transform": xnli
+    },
+    "xnli_de": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "filter_column": "language",
+        "filter_value": "de",
+        "transform": xnli
+    },
+    "xnli_es": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "filter_column": "language",
+        "filter_value": "es",
+        "transform": xnli
+    },
+    "xnli_hi": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "filter_column": "language",
+        "filter_value": "hi",
+        "transform": xnli
+    },
+    "xnli_ru": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "filter_column": "language",
+        "filter_value": "ru",
+        "transform": xnli
+    },
+    "xnli_sw": {
+        "path": "xtreme",
+        "name": "XNLI", 
+        "train_split": "validation", 
+        "test_split": "test", 
+        "filter_column": "language",
+        "filter_value": "sw",
+        "transform": xnli
+    },
+    # --------------------------------------------------------------------------
+    # superglue
+    # --------------------------------------------------------------------------
+    "boolq": {
+        "path": "super_glue",
+        "name": "boolq",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": boolq
+    },
+    "multirc": {
+        "path": "super_glue",
+        "name": "multirc",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": multirc
+    },
+    "cb": {
+        "path": "super_glue",
+        "name": "cb",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": cb
+    },
+    "copa": {
+        "path": "super_glue",
+        "name": "copa",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": copa
+    },
+    "rte": {
+        "path": "super_glue",
+        "name": "rte",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": rte
+    },
+    "wic": {
+        "path": "super_glue",
+        "name": "wic",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": wic
+    },
+    "wsc": {
+        "path": "super_glue",
+        "name": "wsc",
+        "train_split": "train",
+        "test_split": "validation",
+        "transform": wsc
     },
 }
